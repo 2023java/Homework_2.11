b35d1a01233c4d9018012f172c2f30cb6958c4c2
Artnes <2023java@mail.ru>
2023-09-12 17:59:52 +0300

    Homework_2.11 - готовое

---
+++ store/src/main/java/pro/sky/store/service/Cart.java
@@ -0,0 +1,24 @@
+package pro.sky.store.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@SessionScope
+public class Cart {
+    private List<Integer> items;
+
+    public Cart () {
+        items = new ArrayList<>();
+    }
+    public void add (Integer item){
+        items.add(item);
+    }
+
+    public List<Integer> getItems() {
+        return items;
+    }
+}
